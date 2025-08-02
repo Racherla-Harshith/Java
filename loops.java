a27f866025621306430d9175399f2c3b795a899e
Racherla-Harshith <rharshith2005@gmail.com>
2025-08-03 00:05:33 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,48 @@
+// public class loops {
+//     public static void main(String[] args) {
+//         for (int i = 0; i < 100; i=i+1) {
+//             System.out.println("Hello, World!");
+//         }
+//     }
+// }
+
+// public class loops {
+//     public static void main(String[] args) {
+//         for (int i = 0; i < 10; i++) {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// public class loops {
+//     public static void main(String[] args) {
+//         for (int i = 0; i < 11; i++) {
+//             System.out.print(i+" ");
+//         }
+//     }
+// }
+
+//while loop example
+// public class loops {
+//     public static void main(String[] args) {
+//         int i = 0;
+//         while (i < 11) {
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+
+// do-while loop example
+public class loops {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 11); {
+            System.out.println(i);
+        }
+    }
+}
